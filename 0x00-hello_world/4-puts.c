242feb4f5d1493a113f593916f2c086627b280fe
Ugo-Ogbodo <ugojulius.og@gmail.com>
2022-06-02 15:36:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
-/*
- *File:  4-puts.c
- *Auth: Ugo-Ogbodo
- */
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0(Success)
+ * main - main block
+ * Return: 0
  */
-int main(void);
+int main(void)
 {
-  puts("\"programming is like building a multilingual puzzle");
-  
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }

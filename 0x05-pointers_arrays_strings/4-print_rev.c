20daff4bd15232978d385e27b74d306fc8a7031d
Ugo-Ogbodo <ugojulius.og@gmail.com>
2022-06-13 10:16:32 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#includes "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int index = 0;
+
+	while (index >= 0)
+	{
+		if (s[index] == '\0')
+			break;
+		index++;
+	}
+	for (index--; index >= 0; index--)
+		_putchar(s[index]);
+	_putchar('\n');
+}

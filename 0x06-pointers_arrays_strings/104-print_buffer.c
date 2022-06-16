c5448e61238b62a0a37ea67c3309467d5c1e9316
Ugo-Ogbodo <ugojulius.og@gmail.com>
2022-06-16 15:03:23 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,40 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
- *
- * Return: void
- */
-
-void print_line(char *c, int s, int l)
-{
-	int j, k;
-
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
-
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
+ * print_buffer - prints string buffer
+ * @b: the buffer
  * @size: size of buffer
  *
  * Return: void
@@ -42,21 +11,35 @@ void print_line(char *c, int s, int l)
 
 void print_buffer(char *b, int size)
 {
-	int i;
+	int p = 0, i, j;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	if (size <= 0)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
+		printf("\n");
+		return;
 	}
-	if (size == 0)
-		putchar('\n');
+	while (p < sizee)
+	{
+		j = size - p < 10 ? size - p : 10;
+		printf("%08x: ", p);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + p + i));
+			else
+				printf(" ");
+			if (i % 2)
+				printf(" ");
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+				c = '.';
+			printf("%c",  c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }

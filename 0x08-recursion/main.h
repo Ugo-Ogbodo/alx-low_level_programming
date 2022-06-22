40d8c5c41383e3b8b5824d540d344799992aabeb
Ugo-Ogbodo <ugojulius.og@gmail.com>
2022-06-22 10:56:19 -0700

    updated main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int nat_square(int i, int j);
 int is_prime_number(int n);
+int primeNum(int num, int prime);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 

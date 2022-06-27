20b9a34cc24374dd6f04633b3cd93294ab21c795
Ugo-Ogbodo <ugojulius.og@gmail.com>
2022-06-27 08:24:43 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,31 +11,29 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, k, l;
+	int i, j, k, l;
 	char *a;
 
 	if (s1 == NULL)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
+		s1 = " ";
 	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
-	k = i + j + 1;
-	a = malloc(k * sizeof(char));
+		s2 = " ";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
 	if (a == NULL)
 		return (NULL);
-	for (l = 0; l < i; l++)
-		a[1] = s1[l];
-	for (l = 0; l < j; l++)
-		a[l + i] = s2[l];
-	a[i + j] = '\0';
+
+	for (k = 0, l = 0; k < (i + j + 1); k++)
+	{
+		if (k < i)
+			a[k] = s1[k];
+		else
+			a[k] = s2[l++];
+	}
 	return (a);
 }

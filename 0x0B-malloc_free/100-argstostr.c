39cb88d4a0ab44a1dd38451c359d4438750d4056
Ugo-Ogbodo <ugojulius.og@gmail.com>
2022-06-27 10:58:36 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,37 +11,33 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, l;
+	int i, j, k = 0, l = 0;
 	char *a;
 
-	if (ac == 0 || av == NULL)
+	if (ac <= 0 || av == NULL)
 		return (NULL);
-
-	l = 0;
 	for (i = 0; i < ac; i++)
 	{
 
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 			l++;
 		l++;
 	}
+	l++;
 
-	a = malloc((l + 1) * sizeof(char));
+	a = malloc(l * sizeof(char));
 	if (a == NULL)
 		return (NULL);
-
-	k = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j + 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
-			str[k] = av[i][j];
+			a[k] = av[i][j];
 			k++;
 		}
-		str[k] = '\n';
+		a[k] = '\n';
 		k++;
 	}
-	str[k] = '\0';
-
+	a[k] = '\0';
 	return (a);
 }

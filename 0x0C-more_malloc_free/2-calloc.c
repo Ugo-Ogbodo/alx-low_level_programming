9fe12b93914598c750cd422bdc8886c287db5dff
Ugo-Ogbodo <ugojulius.og@gmail.com>
2022-06-30 09:41:46 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *j;
+	char *j;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	j = malloc(nmemb * size);
 	if (j == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		j[i] = 0;
 	return (j);
 }

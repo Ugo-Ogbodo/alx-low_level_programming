22d80ea0fed43ff6e88ec60c26371ccbc0227758
ugo-Ogbodo <ugojulius.og@gmail.com>
2022-07-18 15:23:26 -0700

    linked list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *new;
+	listint_t *next;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		new = (*head)->new;
+		next = (*head)->next;
 		free(*head);
-		*head = new;
+		*head = next;
 	}
 }

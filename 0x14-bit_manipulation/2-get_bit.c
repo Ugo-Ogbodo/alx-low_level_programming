179500a278475567b7e105dba08450ae068d2c8f
ugo-Ogbodo <ugojulius.og@gmail.com>
2022-07-21 08:22:37 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * get_bit - returns the value of a bit at a given index.
  * @n: number to check bits in
  * @index: index at which to check bit
  *
- * Return: value of the bit, or -1 if there is an error
+ * Return: value of the bit, or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
+	unsigned long int div, check;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
+	div = 1 << index;
+	check = n & div;
+	if (check == div)
 		return (1);
 	return (0);
 }
